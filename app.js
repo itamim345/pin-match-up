96729746b4941917b8b0c7f04c5b1731bd5110c1
AF Tamim <itamim345@gmail.com>
2022-05-30 17:42:43 +0530

    completed taking user input

--- app.js
+++ app.js
@@ -11,4 +11,16 @@ document.getElementById('genarate').addEventListener('click', function(){
     document.getElementById('generated-pin').value = getpin();
 })
 
-
+document.getElementById('keypad').addEventListener('click', function(event){
+    const InputNumber = (event.target.innerText);
+    const calcDisplay = document.getElementById('typed-key');
+    if(isNaN(InputNumber)){
+        if(InputNumber == 'C'){
+            calcDisplay.value = "";
+        }
+    }else{
+        const oldNumber = calcDisplay.value;
+        const newNumber = oldNumber+InputNumber;
+        calcDisplay.value = newNumber;
+    }
+})
